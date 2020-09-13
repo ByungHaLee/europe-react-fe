b87b85429b36367b387c9a13a557bcda55d47f18
Byung Ha Lee <byungha.lee@metronom.com>
2020-09-13 16:28:51 +0200

    Second answer

--- src/App.js
+++ src/App.js
@@ -3,56 +3,30 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      todo: ""
+    };
+  }
+  addTodo() {
+    if(this.state.todo)
+      this.setState({list:[...this.state.list, this.state.todo]})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <input type="text" onChange={e=>this.setState({todo:e.target.value})}/>
+          <button onClick={()=>this.addTodo()}>add</button>
+        </div>
+        {this.state.list.map(todo=><li>{todo}</li>)}
+      </div>
+    );
+  }
 }
 
 export default App;
